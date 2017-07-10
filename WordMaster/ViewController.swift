3a83d0401d21ce3e47022e53698259810b93a97d
Nghia Ho <hotg@netvirta.com>
2017-07-10 17:08:14 +0800

    function declarations

--- WordMaster/ViewController.swift
+++ WordMaster/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var labelHint: UILabel!
     @IBOutlet weak var labelNumberOfGuesses: UILabel!
@@ -16,9 +16,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var textInput: UITextField!
     @IBOutlet weak var labelLetterGuessed: UILabel!
     
+    let wordList: [[Character]] = [["m", " ", "a", " ","m", " ", "m", " ", "o", " ", "t", "h"],
+                                   ["f", " ", "o", " ", "o", " ", "t", "b", " ", "a", " ", "l", " ", "l"]]
+    
+    let hintList : [String] = ["extinct animal, 7 letters",
+                               "popular sport, 8 letters"]
+    
+    var randomNumber : Int?
+    var lastRandomNumber : Int?
+    var wordToGuess : [Character] = []
+    var wordToProgress : [Character] = []
+    var score : Int = 5
+    let maxScore : Int = 5
+    var letterBank : String = "Letters guessed: "
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +43,46 @@ class ViewController: UIViewController {
 
 
     @IBAction func pressNewWordButton(_ sender: UIButton) {
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        <#code#>
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        <#code#>
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        <#code#>
+    }
+    
+    func setRandomNumber(){
+        
+    }
+    
+    func setWordToGuessAndHint(){
+        
+    }
+    
+    func setWordProgress(){
+        
+    }
+    
+    func guessLetter(letterGuess : Character){
+        
+    }
+    
+    func revealLetter(_index : Int, _letter : Character){
+        
+    }
+    
+    func addLetterToBank(letter : String){
+        
+    }
+    
+    func checkIfEndGame(){
+        
     }
 }
 
